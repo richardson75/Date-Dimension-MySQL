b9fb7c6d96251f44cf8ec62daa0022cfaaf6c4be
CMarcus <cmarcus.richardson@gmail.com>
2016-05-20 08:46:15 -0700

    Created
    First Commit

---
+++ Create_date_dim.sql
@@ -0,0 +1,27 @@
+drop table if exists dim_date;
+CREATE TABLE dim_date(
+ date_key int NOT NULL,
+ full_date date NULL,
+ date_name char(11) NOT NULL,
+ date_name_us char(11) NOT NULL,
+ date_name_eu char(11) NOT NULL,
+ day_of_week tinyint NOT NULL,
+ day_name_of_week char(10) NOT NULL,
+ day_of_month tinyint NOT NULL,
+ day_of_year smallint NOT NULL,
+ weekday_weekend char(10) NOT NULL,
+ week_of_year tinyint NOT NULL,
+ month_name char(10) NOT NULL,
+ month_of_year tinyint NOT NULL,
+ is_last_day_of_month char(1) NOT NULL,
+ calendar_quarter tinyint NOT NULL,
+ calendar_year smallint NOT NULL,
+ calendar_year_month char(10) NOT NULL,
+ calendar_year_qtr char(10) NOT NULL,
+ fiscal_month_of_year tinyint NOT NULL,
+ fiscal_quarter tinyint NOT NULL,
+ fiscal_year int NOT NULL,
+ fiscal_year_month char(10) NOT NULL,
+ fiscal_year_qtr char(10) NOT NULL,
+  PRIMARY KEY (`date_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
